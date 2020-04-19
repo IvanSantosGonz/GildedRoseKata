37c8d0d1d8c253fe6b6a2ba0a0b63ad7d8191271
Iván Santos <ivansantosgonz@gmail.com>
2020-04-19 15:34:13 +0200

    REFACTOR GREEN: Open Close, liskov Substitution, polymorphism: change tests to use class equality

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -6,21 +6,19 @@ namespace csharp {
     public class GildedRoseTest {
         [Test]
         public void At_the_end_of_each_day_roses_lowers_quality_and_sell_in() {
-            var rose = new Rose {Name = "A normal rose", SellIn = 28, Quality = 40};
+            var rose = new Rose("A normal rose", 28, 40);
             IList<Rose> roses = new List<Rose> {rose};
             GildedRose gildedRose = new GildedRose(roses);
 
             gildedRose.UpdateQuality();
 
-            var roseAfterFiveDays = new Rose {Name = "A normal rose", SellIn = 27, Quality = 39};
-            Assert.AreEqual(roseAfterFiveDays.Name, roses[0].Name);
-            Assert.AreEqual(roseAfterFiveDays.SellIn, roses[0].SellIn);
-            Assert.AreEqual(roseAfterFiveDays.Quality, roses[0].Quality);
+            var roseAfterFiveDays = new Rose("A normal rose", 27, 39);
+            Assert.True(roseAfterFiveDays.Equals(rose));
         }
-        
+
         [Test]
         public void Once_the_sell_by_date_has_passed_Quality_degrades_twice_as_fast() {
-            var rose = new Rose {Name = "A normal rose", SellIn = 0, Quality = 40};
+            var rose = new Rose("A normal rose", 0, 40);
             IList<Rose> roses = new List<Rose> {rose};
             GildedRose gildedRose = new GildedRose(roses);
 
@@ -28,15 +26,13 @@ namespace csharp {
                 gildedRose.UpdateQuality();
             }
 
-            var roseAfterFiveDays = new Rose {Name = "A normal rose", SellIn = -5, Quality = 30};
-            Assert.AreEqual(roseAfterFiveDays.Name, roses[0].Name);
-            Assert.AreEqual(roseAfterFiveDays.SellIn, roses[0].SellIn);
-            Assert.AreEqual(roseAfterFiveDays.Quality, roses[0].Quality);
+            var roseAfterFiveDays = new Rose("A normal rose", -5, 30);
+            Assert.True(roseAfterFiveDays.Equals(rose));
         }
-        
+
         [Test]
         public void the_quality_of_a_rose_is_never_negative() {
-            var rose = new Rose {Name = "Elixir of the Mongoose", SellIn = 0, Quality = 0};
+            var rose = new Rose("Elixir of the Mongoose", 0, 0);
             IList<Rose> roses = new List<Rose> {rose};
             GildedRose gildedRose = new GildedRose(roses);
 
@@ -44,12 +40,10 @@ namespace csharp {
                 gildedRose.UpdateQuality();
             }
 
-            var roseAfterFiveDays = new Rose {Name = "Elixir of the Mongoose", SellIn = -5, Quality = 0};
-            Assert.AreEqual(roseAfterFiveDays.Name, roses[0].Name);
-            Assert.AreEqual(roseAfterFiveDays.SellIn, roses[0].SellIn);
-            Assert.AreEqual(roseAfterFiveDays.Quality, roses[0].Quality);
+            var roseAfterFiveDays = new Rose("Elixir of the Mongoose", -5, 0);
+            Assert.True(roseAfterFiveDays.Equals(rose));
         }
-        
+
         [Test]
         public void Aged_Brie_rose_actually_increases_in_Quality_the_older_it_gets() {
             var rose = new AgedBrie(5, 35);
@@ -60,12 +54,10 @@ namespace csharp {
                 gildedRose.UpdateQuality();
             }
 
-            var roseAfterFiveDays = new Rose {Name = "Aged Brie", SellIn = 0, Quality = 40};
-            Assert.AreEqual(roseAfterFiveDays.Name, roses[0].Name);
-            Assert.AreEqual(roseAfterFiveDays.SellIn, roses[0].SellIn);
-            Assert.AreEqual(roseAfterFiveDays.Quality, roses[0].Quality);
+            //It is done so to test equality between child and parent
+            var roseAfterFiveDays = new Rose("Aged Brie", 0, 40);
         }
-        
+
         [Test]
         public void The_quality_of_a_rose_is_never_more_than_50() {
             var rose = new AgedBrie(0, 50);
@@ -76,68 +68,59 @@ namespace csharp {
                 gildedRose.UpdateQuality();
             }
 
-            var roseAfterFiveDays = new Rose {Name = "Aged Brie", SellIn = -5, Quality = 50};
-            Assert.AreEqual(roseAfterFiveDays.Name, roses[0].Name);
-            Assert.AreEqual(roseAfterFiveDays.SellIn, roses[0].SellIn);
-            Assert.AreEqual(roseAfterFiveDays.Quality, roses[0].Quality);
+            var roseAfterFiveDays = new Rose("Aged Brie", -5, 50);
+            Assert.True(roseAfterFiveDays.Equals(rose));
         }
-        
+
         [Test]
         public void Sulfuras_are_a_legendary_rose_and_never_has_to_be_sold_or_decrease_their_quality() {
             var rose = new Sulfuras(0, 40);
             IList<Rose> roses = new List<Rose> {rose};
-            GildedRose gildedRose = new GildedRose(roses);
 
+            GildedRose gildedRose = new GildedRose(roses);
             for (var i = 0; i < 5; i++) {
                 gildedRose.UpdateQuality();
             }
 
-            var roseAfterFiveDays = new Rose {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 40};
-            Assert.AreEqual(roseAfterFiveDays.Name, roses[0].Name);
-            Assert.AreEqual(roseAfterFiveDays.SellIn, roses[0].SellIn);
-            Assert.AreEqual(roseAfterFiveDays.Quality, roses[0].Quality);
+            var roseAfterFiveDays = new Rose("Sulfuras, Hand of Ragnaros", 0, 40);
+            Assert.True(roseAfterFiveDays.Equals(rose));
         }
-        
+
         [Test]
-        public void Backstage_passes_like_aged_brie_by_2_when_there_are_10_days_or_less_and_by_3_when_there_are_5_days_or_less() {
+        public void
+            Backstage_passes_like_aged_brie_by_2_when_there_are_10_days_or_less_and_by_3_when_there_are_5_days_or_less() {
             var rose = new BackstagePass(10, 20);
             IList<Rose> roses = new List<Rose> {rose};
+
             GildedRose gildedRose = new GildedRose(roses);
+            for (var i = 0; i < 5; i++) {
+                gildedRose.UpdateQuality();
+            }
+
+            var roseAfterFiveDays = new Rose("Backstage passes to a TAFKAL80ETC concert", 5, 31);
+            Assert.True(roseAfterFiveDays.Equals(rose));
 
             for (var i = 0; i < 5; i++) {
                 gildedRose.UpdateQuality();
             }
 
-            var roseAfterFiveDays = new Rose {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 31};
-            Assert.AreEqual(roseAfterFiveDays.Name, roses[0].Name);
-            Assert.AreEqual(roseAfterFiveDays.SellIn, roses[0].SellIn);
-            Assert.AreEqual(roseAfterFiveDays.Quality, roses[0].Quality);
-            
-            for (var i = 0; i < 5; i++) {
-                gildedRose.UpdateQuality();
-            }
+            roseAfterFiveDays = new Rose("Backstage passes to a TAFKAL80ETC concert", 0, 46);
 
-            roseAfterFiveDays = new Rose {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 46};
-            Assert.AreEqual(roseAfterFiveDays.Name, roses[0].Name);
-            Assert.AreEqual(roseAfterFiveDays.SellIn, roses[0].SellIn);
-            Assert.AreEqual(roseAfterFiveDays.Quality, roses[0].Quality);
+            Assert.True(roseAfterFiveDays.Equals(rose));
         }
 
         [Test]
         public void Backstage_passes_like_aged_brie_gets_quality_0_after_concert() {
             var rose = new BackstagePass(10, 20);
             IList<Rose> roses = new List<Rose> {rose};
-            GildedRose gildedRose = new GildedRose(roses);
 
+            GildedRose gildedRose = new GildedRose(roses);
             for (var i = 0; i < 11; i++) {
                 gildedRose.UpdateQuality();
             }
 
-            var roseAfterFiveDays = new Rose {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -1, Quality = 0};
-            Assert.AreEqual(roseAfterFiveDays.Name, roses[0].Name);
-            Assert.AreEqual(roseAfterFiveDays.SellIn, roses[0].SellIn);
-            Assert.AreEqual(roseAfterFiveDays.Quality, roses[0].Quality);
+            var roseAfterFiveDays = new Rose("Backstage passes to a TAFKAL80ETC concert", -1, 0);
+            Assert.True(roseAfterFiveDays.Equals(rose));
         }
-
     }
 }
--- Rose.cs
+++ Rose.cs
@@ -16,6 +16,26 @@
             Quality = quality;
         }
 
+        public bool Equals(Rose other) {
+            return Name == other.Name && SellIn == other.SellIn && Quality == other.Quality;
+        }
+
+        public override bool Equals(object obj) {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Rose) obj);
+        }
+
+        public override int GetHashCode() {
+            unchecked {
+                var hashCode = (Name != null ? Name.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ SellIn;
+                hashCode = (hashCode * 397) ^ Quality;
+                return hashCode;
+            }
+        }
+
         public override string ToString()
         {
             return this.Name + ", " + this.SellIn + ", " + this.Quality;
